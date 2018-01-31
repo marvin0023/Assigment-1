be5b73c708da33ae49b45cbc24c0336e940d658d
Marvin Castro <cdcastro@hotmail.es>
2018-01-30 21:54:29 -0800

    First Commit

---
+++ circuference.cpp
@@ -0,0 +1,19 @@
+#include<studio>
+#include<stdint.h>
+
+extern "C" double radius_io();
+extern "C" double area_io();
+extern "C" double circumference_io();
+
+int main()
+{
+	double return_radius = -99.99;
+	double return_area = -99.99;
+	double return_circumference = -99.99;
+	
+	printf("%s", "Welcome to the circle program. This program will compute the area and the circumference of a circle: \n");
+
+
+	
+	return 0;
+}
