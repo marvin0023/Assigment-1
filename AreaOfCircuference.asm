22b1a6b2b9716471ad4f4c7dca998ebb827bf97f
Marvin Castro <cdcastro@hotmail.es>
2018-02-13 20:45:20 -0800

    work

--- AreaOfCircuference.asm
+++ AreaOfCircuference.asm
@@ -6,7 +6,7 @@ global Circle
 segment .data
 Circle.initialmessage db "Please enter the radius of the circle: ", 0
 
-Circle.radiousMessage db "The circle with radius ", 0
+Circle.radiousMessage db "The circle with radius %1.18lf", 0
 
 Stringformat db "%s", 0
 
@@ -32,20 +32,30 @@ push 		qword 0
 mov qword	rax, 0
 mov			rdi, eight_byte_format
 mov			rsi, rsp
-call    scanf
-movsd   xmm0, [rsp]
+call    	scanf
+movsd   	xmm0, [rsp]
 pop			rax
+
+movsd xmm1, xmm0
+movsd xmm2, xmm0
+
+
+mov rbx, 0x400921FB54442D18
+push rbx
+
+
+
 
 push qword 0
 movsd [rsp], xmm0
 
-push qword -1
-mov rax, 0x4008000000000000
-mov rdi, .radiousMessage
-call printf
-pop rax	
+push 	qword -1
+mov 	rax, 1
+mov		rdi, .radiousMessage
+call 	printf
+pop 	rax	
+pop rax
 
-
-pop 		rbp
-res
+pop 	rbp
+ret
 
